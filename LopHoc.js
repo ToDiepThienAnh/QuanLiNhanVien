3577edc07fe09d6728009753257ad9069302a218
unknown <phannhatthinh99@gmail.com>
2020-11-08 16:42:46 +0700

    thinh

--- LopHoc.js
+++ LopHoc.js
@@ -1,10 +1,22 @@
 var LopHoc = function() {
-    this.danhSachSinhVien = [];
-    // user A
-    this.layDanhSachSinhVien = function() {
+        this.danhSachSinhVien = [];
+        // Thiên Anh did it
+        this.mota = 'Đây là lớp FE4 đang học git';
         //Thinh did it
         this.tenLopHoc = 'FE54';
         this.tenHocSinh = 'Thinh';
-        return this.danhSachSinhVien;
-    }
-}
+        // user B
+        console.log(123)
+            // userMinh
+
+        this.moTa = 'Đây là lớp fe54 đang học git';
+
+        this.layDanhSachSinhVien = function() {
+
+            this.layDanhSachSinhVien = function() {
+                //Thinh did it
+                this.tenLopHoc = 'FE54';
+                this.tenHocSinh = 'Thinh';
+                return this.danhSachSinhVien;
+            }
+        }
---
+++ MonHoc.js
@@ -0,0 +1,3 @@
+var MonHoc = function() {
+    this.tenMonHoc = 'LapTrinhWeb';
+}

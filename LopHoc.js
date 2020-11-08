775837980aefa7e43ba82e5e038b48c7db68595a
unknown <todiepthienanh@gmail.com>
2020-11-08 15:48:42 +0700

    'ThAnh-Fixed'

--- LopHoc.js
+++ LopHoc.js
@@ -2,10 +2,11 @@ var LopHoc = function(){
     this.danhSachSinhVien = [];
     // Thiên Anh did it
     this.mota = 'Đây là lớp FE4 đang học git';
+    //Thinh did it
+    this.tenLopHoc= 'FE54';
+    this.tenHocSinh='Thinh';
     this.layDanhSachSinhVien = function(){
-        //Thinh did it
-        this.tenLopHoc= 'FE54';
-        this.tenHocSinh='Thinh';
+        
         return this.danhSachSinhVien;
     }
 }

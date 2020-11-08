60fe440cef5ab28f0cb7b99a8c07d4dd02525531
Đặng Phúc Minh <s2vethan2s@gmail.com>
2020-11-08 15:46:34 +0700

    Minh

--- LopHoc.js
+++ LopHoc.js
@@ -1,5 +1,7 @@
 var LopHoc = function(){
     this.danhSachSinhVien = [];
+    // userA
+    this.moTa = 'Đây là lớp fe54 đang học git';
     this.layDanhSachSinhVien = function(){
         return this.danhSachSinhVien;
     }

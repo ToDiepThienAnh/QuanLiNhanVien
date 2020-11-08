7242ab3819506abb09b971644229b86a5db9e8bd
unknown <phannhatthinh99@gmail.com>
2020-11-08 15:45:02 +0700

    thinh

--- LopHoc.js
+++ LopHoc.js
@@ -1,6 +1,9 @@
 var LopHoc = function(){
     this.danhSachSinhVien = [];
     this.layDanhSachSinhVien = function(){
+        //Thinh did it
+        this.tenLopHoc= 'FE54';
+        this.tenHocSinh='Thinh';
         return this.danhSachSinhVien;
     }
 }

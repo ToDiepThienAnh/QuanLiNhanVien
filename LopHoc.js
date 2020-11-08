4d3326f6d8a23800133226efda186843c08e1288
unknown <todiepthienanh@gmail.com>
2020-11-08 15:45:50 +0700

    'Thien-Anh'

--- LopHoc.js
+++ LopHoc.js
@@ -1,5 +1,7 @@
 var LopHoc = function(){
     this.danhSachSinhVien = [];
+    // Thiên Anh did it
+    this.mota = 'Đây là lớp FE4 đang học git';
     this.layDanhSachSinhVien = function(){
         return this.danhSachSinhVien;
     }
